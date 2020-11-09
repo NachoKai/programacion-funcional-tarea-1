d606590db607447da31e5f8cd1d83e944965e48e
Nacho Caiafa <nachokai@outlook.com>
2020-11-09 20:03:37 -0300

    resolve some exercises

--- src/ejercicios.js
+++ src/ejercicios.js
@@ -1,18 +1,30 @@
-export function multiplicarPor10(array) {}
+export function multiplicarPor10(array) {
+  return array.map(n => n * 10);
+}
 
-export function soloPares(array) {}
+export function soloPares(array) {
+  return array.filter(n => n % 2 === 0);
+}
 
 export function moverALaDerecha(array) {}
 
-export function soloUnaPalabra(array) {}
+export function soloUnaPalabra(array) {
+  return array.filter(e => e.split(" ").length === 1);
+}
 
-export function soloArraysPositivos(array) {}
+export function soloArraysPositivos(array) {
+  return array.filter(a => a.every(n => n >= 0));
+}
 
-export function soloVocales(array) {}
+export function soloVocales(array) {
+  return array.map(w => [...w].filter(l => l.match(/a|e|i|o|u/)).join(""));
+}
 
 export function todasVocalesIguales(array) {}
 
-export function duplicarMatriz(array) {}
+export function duplicarMatriz(array) {
+  return array.map(a => a.map(n => n * 2));
+}
 
 export function mayoresDeEdadValidados(array) {}
 
