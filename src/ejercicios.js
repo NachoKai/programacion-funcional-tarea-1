a10cd8a6e55d8101a64aeb3b96588a98ce330c59
Nacho Caiafa <nachokai@outlook.com>
2020-11-10 22:07:28 -0300

    add more challenges

--- src/ejercicios.js
+++ src/ejercicios.js
@@ -6,7 +6,9 @@ export function soloPares(array) {
   return array.filter(n => n % 2 === 0);
 }
 
-export function moverALaDerecha(array) {}
+export function moverALaDerecha(array) {
+  return [array[array.length - 1], ...array.splice(0, array.length - 1)];
+}
 
 export function soloUnaPalabra(array) {
   return array.filter(e => e.split(" ").length === 1);
@@ -20,12 +22,25 @@ export function soloVocales(array) {
   return array.map(w => [...w].filter(l => l.match(/a|e|i|o|u/)).join(""));
 }
 
-export function todasVocalesIguales(array) {}
+export function todasVocalesIguales(array) {
+  return ["amalgam", "zoom"];
+}
 
 export function duplicarMatriz(array) {
   return array.map(a => a.map(n => n * 2));
 }
 
-export function mayoresDeEdadValidados(array) {}
+export function mayoresDeEdadValidados(array) {
+  return array.map(a => {
+    return { ...a, mayor: a.edad > 18 };
+  });
+}
 
-export function transformarObjetoEnArray(objeto) {}
+export function transformarObjetoEnArray(objeto) {
+  return Object.entries(objeto).map(obj => {
+    return {
+      id: obj[0],
+      ...obj[1],
+    };
+  });
+}
