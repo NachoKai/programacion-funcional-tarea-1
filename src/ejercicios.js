ef7d40fb8857779a94f141cf58864ada9bf8921d
Nacho Caiafa <nachokai@outlook.com>
2020-11-10 22:09:48 -0300

    add more challenges

--- src/ejercicios.js
+++ src/ejercicios.js
@@ -1,9 +1,9 @@
 export function multiplicarPor10(array) {
-  return array.map(n => n * 10);
+  return array.map(num => num * 10);
 }
 
 export function soloPares(array) {
-  return array.filter(n => n % 2 === 0);
+  return array.filter(num => num % 2 === 0);
 }
 
 export function moverALaDerecha(array) {
@@ -11,28 +11,27 @@ export function moverALaDerecha(array) {
 }
 
 export function soloUnaPalabra(array) {
-  return array.filter(e => e.split(" ").length === 1);
+  return array.filter(word => word.split(" ").length === 1);
 }
 
 export function soloArraysPositivos(array) {
-  return array.filter(a => a.every(n => n >= 0));
+  return array.filter(num => num.every(n => n >= 0));
 }
 
 export function soloVocales(array) {
-  return array.map(w => [...w].filter(l => l.match(/a|e|i|o|u/)).join(""));
+  return array.map(word => [...word].filter(letter => letter.match(/a|e|i|o|u/)).join(""));
 }
 
 export function todasVocalesIguales(array) {
-  return ["amalgam", "zoom"];
 }
 
 export function duplicarMatriz(array) {
-  return array.map(a => a.map(n => n * 2));
+  return array.map(arr => arr.map(num => num * 2));
 }
 
 export function mayoresDeEdadValidados(array) {
-  return array.map(a => {
-    return { ...a, mayor: a.edad > 18 };
+  return array.map(arr => {
+    return { ...arr, mayor: arr.edad > 18 };
   });
 }
 
